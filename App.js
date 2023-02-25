ed666b63e0d030f9038813e4af832a4cd37e4104
solemannoushad <solemannoushad31@gmail.com>
2023-02-25 22:51:11 +0500

    second

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from "react-native";
-const logo = require("./assets/fbLogo.svg");
+const logo = require("./assets/fb.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.logoMain}>
         <Image style={styles.logo} source={logo} />
-        <Text style={{fontSize: 24 , marginLeft: '70px' , marginRight: '70px' , textAlign: 'center' , fontWeight: '400'}}>Facebook helps you connect and share with the people in your life</Text>
+        <Text style={{fontSize: 22 , marginLeft: 50 , marginRight: 50 , textAlign: 'center' , fontWeight: '400'}}>Facebook helps you connect and share with the people in your life</Text>
       </View>
       <View style={styles.inputMain}>
         <TextInput style={styles.input}  placeholder="Email address or phone number" />
@@ -14,12 +14,13 @@ export default function App() {
         <TouchableOpacity style={styles.button}>
         <Text style={styles.btnText}>Log In</Text>
       </TouchableOpacity>
-        <Text style={{color: '#1877f2' , marginTop: '10px'}}>Forgetten Password?</Text>
+        <Text style={{color: '#1877f2' , marginTop: 10}}>Forgetten Password?</Text>
       </View>
       <View style={styles.btnMain}>
       <TouchableOpacity style={styles.buttonNew}>
         <Text style={styles.btnText}>Create new account</Text>
       </TouchableOpacity>
+      <Text style={{marginTop: 20}}><Text style={{fontWeight: 600}}>Create a Page</Text> for a celebrity, brand or business.</Text>
       </View>
     </View>
   );
@@ -37,23 +38,23 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   logo: {
-    width: 300,
+    width: 200,
     height: 100,
   },
   inputMain: {
     flex: 0.3,
     width: "100%",
     alignItems: "center",
-    marginTop: '10px',
+    marginTop: 20,
   },
   input: {
     borderWidth: 1,
     borderColor: '#dddfe2',
     color: 'grey',
-    marginBottom: '18px',
-    borderRadius: '5px',
-    padding: '10px',
-    width: 350,
+    marginBottom: 18,
+    borderRadius: 5,
+    padding: 10,
+    width: 300,
     fontSize: 15,
   },
   btnMain: {
@@ -63,21 +64,25 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: '#1877f2',
-    width: 350,
+    width: 300,
     textAlign: 'center',
-    padding: '10px',
-    borderRadius: '5px'
+    padding: 10,
+    borderRadius: 5
   },
   btnText: {
     color: '#fff',
     fontWeight: 'bold',
-    fontSize: '17px'
+    fontSize: 17,
+    alignItems: 'center',
+    textAlign: 'center'
   },
   buttonNew: {
     backgroundColor: '#42b72a',
-    width: 180,
+    width: 200,
     textAlign: 'center',
-    padding: '10px',
-    borderRadius: '5px'
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 20
   },
 });
